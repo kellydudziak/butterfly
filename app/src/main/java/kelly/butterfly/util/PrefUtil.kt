f547e2f37594b8aa1816535b74a806dd2acaff3c
kellydudziak <kellydudziak@gmail.com>
2019-10-04 20:30:06 -0700

    Initial commit.

---
+++ app/src/main/java/kelly/butterfly/util/PrefUtil.kt
@@ -0,0 +1,55 @@
+package kelly.butterfly.util
+
+import android.content.Context
+import android.preference.PreferenceManager
+import kelly.butterfly.MainActivity
+
+class PrefUtil {
+    companion object {
+
+        fun getTimerLength(context: Context): Int {
+            // placeholder
+            return 1
+        }
+
+        private const val PREVIOUS_TIMER_LENGTH_SECONDS_TO = "kelly.butterfly.previous_timer_length"
+
+        fun getPreviousTimerLengthSeconds(context: Context): Long {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getLong(PREVIOUS_TIMER_LENGTH_SECONDS_TO, 0)
+        }
+
+        fun setPreviousTimerLengthSeconds(seconds: Long, context: Context) {
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            editor.putLong(PREVIOUS_TIMER_LENGTH_SECONDS_TO, seconds)
+            editor.apply()
+        }
+
+        private const val TIMER_STATE_ID = "kelly.butterfly.timer_state"
+
+        fun getTimerState(context: Context): MainActivity.TimerState{
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            val ordinal = preferences.getInt(TIMER_STATE_ID, 0)
+            return MainActivity.TimerState.values()[ordinal]
+        }
+
+        fun setTimerState(state: MainActivity.TimerState, context: Context) {
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            val ordinal = state.ordinal
+            editor.putInt(TIMER_STATE_ID, ordinal)
+            editor.apply()
+        }
+
+        private const val SECONDS_REMAINING_ID = "kelly.butterfly.seconds_remaining"
+        fun getSecondsRemaining(context: Context): Long {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getLong(SECONDS_REMAINING_ID, 0)
+        }
+
+        fun setSecondsRemaining(seconds: Long, context: Context) {
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            editor.putLong(SECONDS_REMAINING_ID, seconds)
+            editor.apply()
+        }
+    }
+}
